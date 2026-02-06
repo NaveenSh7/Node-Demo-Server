8f08fe34ccf67d576ffdd2d0cf0fb230783092f8
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2026-02-07 00:16:04 +0530

    changes

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+
 require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
 const Message =  process.env.MESSAGE ;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Cloud Run 🚀");
+  res.send("Hello from Node.js on Cloud Run ");
 });
-console.log(Message)
+
+
 app.get("/message", (req, res) => {
    res.send(`This is from env : ${Message}`);
 });
